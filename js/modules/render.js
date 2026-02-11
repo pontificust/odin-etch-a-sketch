8f09c105dc387024272f7cfa0b6b36e540fd94fe
Pontificust <lihtinmann88@yandex.ru>
2026-02-11 14:28:25 +0300

    feat: add addSketchCells() function
    The function create and append actual number of div elements
    inside sketchContainer.

--- js/modules/render.js
+++ js/modules/render.js
@@ -1,3 +1,14 @@
 export const render = () => {
-    
+    const sketchContainer = document.querySelector('.sketch__container');
+    let cellCount = 16
+
+    const addSketchCells = () => {
+        for(let i = 0; i < cellCount ** 2; i += 1) {
+            const sketchBox = document.createElement('div');
+            sketchBox.classList.add('sketch__box');
+            sketchContainer.appendChild(sketchBox);
+        }
+    }
+    addSketchCells();
+
 }
