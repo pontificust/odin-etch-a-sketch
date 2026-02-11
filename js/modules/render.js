eab5679e03135315a1f2ca316a4b94e030e390ab
Pontificust <lihtinmann88@yandex.ru>
2026-02-11 15:10:33 +0300

    feat: implement reset button click handler
    When the user clicks the reset button, the grid is cleared from colors.

--- js/modules/render.js
+++ js/modules/render.js
@@ -1,10 +1,16 @@
 export const render = () => {
     const sketchContainer = document.querySelector('.sketch__container');
     const sketchGridResize = document.querySelector('.sketch__input-grid-size');
+    const sketchReset = document.querySelector('.sketch__button-reset');
     let cellCount = 16;
 
     const clearContainer = () => {
         sketchContainer.innerHTML = '';
+    }
+
+    const clearColor = () => {
+        const sketchCells = document.querySelectorAll('.sketch__cell');
+        sketchCells.forEach(cell => cell.classList.remove('add-color'));
     }
 
     const addSketchCells = () => {
@@ -24,10 +30,12 @@ export const render = () => {
     }
 
     const draw = (e) => {
-        e.stopPropagation();
-        e.target.classList.add('add-color');
+        if(e.target !== sketchContainer){
+            e.target.classList.add('add-color');
+        }
     }
 
     sketchGridResize.addEventListener('input', gridResize);
     sketchContainer.addEventListener('mouseover', draw);
+    sketchReset.addEventListener('click', clearColor);
 }
