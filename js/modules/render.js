c63c069386c6596dc545c9c9ad6ae9e90bd9a259
Pontificust <lihtinmann88@yandex.ru>
2026-02-11 14:48:27 +0300

    feat: implement input range event handler
    When the user change the value of range the grid
    resizes.

--- js/modules/render.js
+++ js/modules/render.js
@@ -1,14 +1,27 @@
 export const render = () => {
     const sketchContainer = document.querySelector('.sketch__container');
-    let cellCount = 16
+    const sketchGridResize = document.querySelector('.sketch__input-grid-size');
+    let cellCount = 16;
+
+    const clearContainer = () => {
+        sketchContainer.innerHTML = '';
+    }
 
     const addSketchCells = () => {
+        clearContainer();
         for(let i = 0; i < cellCount ** 2; i += 1) {
-            const sketchBox = document.createElement('div');
-            sketchBox.classList.add('sketch__box');
-            sketchContainer.appendChild(sketchBox);
+            const sketchCell = document.createElement('div');
+            sketchCell.classList.add('sketch__cell');
+            sketchContainer.appendChild(sketchCell);
         }
     }
     addSketchCells();
 
+    const gridResize = (e) => {
+        cellCount = e.target.value;
+        sketchContainer.style.setProperty('--cell-size', cellCount);
+        addSketchCells();
+    }
+
+    sketchGridResize.addEventListener('input', gridResize);
 }
